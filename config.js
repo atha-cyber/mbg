6681ff371f6d4738b9eeddda1dbff7dcbdc4ca82
atha-cyber <72985550+atha-cyber@users.noreply.github.com>
2025-12-24 17:04:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,1 @@
-// SIMPAN FILE INI DENGAN NAMA: config.js
-// JANGAN UNGGAH KE GITHUB!
 
-window.firebaseConfig = {
-    apiKey: "AIzaSyCFoiJXBRwJkCmAVSOVb0za1vLlB58Eugc",
-    authDomain: "mbg4-329e3.firebaseapp.com",
-    projectId: "mbg4-329e3",
-    storageBucket: "mbg4-329e3.appspot.com",
-    messagingSenderId: "13330785414",
-    appId: "1:13330785414:web:99505859877a60a2311d07"
-};
-
-window.appSecret = "MBG4PANDAK";
